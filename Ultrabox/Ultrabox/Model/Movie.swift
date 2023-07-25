27ab6736545522e9cdcdf65dd5e294573da0190f
yujinkim1 <yujin7621@me.com>
2023-07-25 21:15:06 +0900

    add(model): Movie structure

---
+++ Ultrabox/Ultrabox/Model/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Ultrabox
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+
+import Foundation
+
+struct Movie {
+	let title: String
+	let runningTime: String
+	let category: String
+	let plot: String
+	let price: Double
+}
