ba54d85ea44d6553cac1ce1ffa93ea0458b10df4
yujinkim1 <yujin7621@me.com>
2023-07-25 21:15:59 +0900

    add(model): User structure

---
+++ Ultrabox/Ultrabox/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Ultrabox
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+
+import Foundation
+
+struct User {
+	let name: String
+	let cash: Double
+	let reservedMovie: [Movie]
+	// let boughtFood: [Food]
+}
