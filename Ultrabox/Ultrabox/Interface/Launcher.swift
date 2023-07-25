5f6a597868cf52267a20d4ee778e4b1e7071ab36
yujinkim1 <yujin7621@me.com>
2023-07-25 09:12:34 +0900

    add: Ultrabox console app

---
+++ Ultrabox/Ultrabox/Interface/Launcher.swift
@@ -0,0 +1,13 @@
+//
+//  Launcher.swift
+//  Ultrabox
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+
+import Foundation
+
+func app() {
+	//MARK: Run console without debug build
+	print("Ultrabox")
+}
---
+++ Ultrabox/Ultrabox/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Ultrabox
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+
+import Foundation
+
+app()
