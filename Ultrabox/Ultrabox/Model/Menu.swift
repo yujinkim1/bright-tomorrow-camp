5869f1fa7957e8024217594850788a18a584e958
yujinkim1 <yujin7621@me.com>
2023-07-25 21:15:39 +0900

    add(model): Menu structure

---
+++ Ultrabox/Ultrabox/Model/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  Ultrabox
+//
+//  Created by Yujin Kim on 2023-07-25.
+//
+/**
+ * 1. 현재 상영작 모두 보기 -> 영화 내역을 모두 볼 수 있음
+ * 2. 
+ */
+import Foundation
+
+struct Menu {
+	let title: String
+	let description: String
+}
