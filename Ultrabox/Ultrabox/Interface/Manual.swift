09dac9508a5da707e11ced46682aa9705d51003c
yujinkim1 <yujin7621@me.com>
2023-07-26 14:03:26 +0900

    modified: Changed manual values

--- Ultrabox/Ultrabox/Interface/Manual.swift
+++ Ultrabox/Ultrabox/Interface/Manual.swift
@@ -8,10 +8,9 @@
 import Foundation
 
 struct Manual {
-	let yourName: String = "이름을 입력해주세요."
-	let on: String = "*** Ultrabox Speed order ***"
+	let auth: String = "이름을 입력해주세요."
+	let wakeup: String = "/ / / Ultrabox Speed order / / /"
 	let selectMenu: String = "아래 항목 중 원하시는 메뉴를 선택해주세요."
-	let selectOption: String = "아래 항목 중 하나를 선택해주세요."
-	let off: String = "*** 이용해 주셔서 감사합니다. ***"
-	let loading: String = "*** 처리 중입니다... ***"
+	let sleep: String = "=====이용해 주셔서 감사합니다.====="
+	let loading: String = "=====처리 중입니다..====="
 }
