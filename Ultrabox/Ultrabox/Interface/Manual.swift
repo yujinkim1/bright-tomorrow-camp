bd75180ed91329a54b15873c27787a232f9f18a0
yujinkim1 <yujin7621@me.com>
2023-07-28 11:47:50 +0900

    modified: Changed manual letters

--- Ultrabox/Ultrabox/Interface/Manual.swift
+++ Ultrabox/Ultrabox/Interface/Manual.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 struct Manual {
-	let auth: String = "이름을 입력해주세요."
-	let wakeup: String = "/ / / Ultrabox Speed order / / /"
-	let selectMenu: String = "아래 항목 중 원하시는 메뉴를 선택해주세요."
-	let sleep: String = "=====이용해 주셔서 감사합니다.====="
-	let loading: String = "=====처리 중입니다..====="
+	let wakeup: String = "/ / / 울트라박스 스피드 오더 / / /"
+	let sleep: String = "=====이용해 주셔서 감사합니다====="
+	let inputLine: String = ">:"
+	let isHomeMenu: String = "=====아래 항목 중 원하시는 메뉴를 선택해주세요====="
+	let isSnack: String = "/ / / 울트라 스낵 메뉴 / / /"
+	let puts: String = "여러 개를 담고 싶다면 번호, 번호 형태로 입력하세요."
+	let isCartEmpty: String = "장바구니가 비어 있습니다."
+	let isWrong: String = "=====입력이 잘못되었습니다 다시 입력해주세요====="
+	let isLoading: String = "=====처리 중입니다====="
 }
