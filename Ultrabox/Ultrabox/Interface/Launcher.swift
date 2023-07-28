e60c7889f3deb810cc2b5dc3a16a2063f7561e6f
yujinkim1 <yujin7621@me.com>
2023-07-28 11:50:14 +0900

    add: Snack layout and comments

--- Ultrabox/Ultrabox/Interface/Launcher.swift
+++ Ultrabox/Ultrabox/Interface/Launcher.swift
@@ -10,12 +10,9 @@ import Foundation
 let manual = Manual()
 let layout = Layout()
 let deposit = Deposit()
-let authenticate = Authenticate()
-var user = User(name: "김유진", cash: deposit.randomCash(), reservedMovie: [])
+var user = User(name: "김유진", cash: deposit.randomCash(), reservedMovie: [], snacks: [])
 // MARK: [app]을 실행
 func app() {
-	// print(manual.auth)
-	// guard let user: User = authenticate.setUser() else { return }
 	// 기본 텍스트 인터페이스를 표시합니다.
 	print(manual.wakeup)
 	print(layout.shrink())
@@ -25,20 +22,24 @@ func app() {
 	layout.showHome()
 	// 사용자는 정해진 입력방식을 통해 원하는 기능을 사용할 수 있습니다.
 	while true {
+		print(manual.inputLine, terminator: " ")
 		guard let selected = readLine()?.uppercased() else {
-			print("번호가 잘못되었습니다. 다시 선택해주세요.")
+			print(manual.isWrong)
 			continue
 		}
 		switch selected {
 			case "1":
+				// 사용자가 1 입력 시 [Layout] 영화 목록을 볼 수 있습니다.
 				layout.showCurrentMovies()
 			case "2":
+				// 사용자가 4 입력 시 [Layout] 예매를 진행할 수 있습니다.
 				layout.selectMovie()
 			case "3":
+				// 사용자가 3 입력 시 [Layout] 최근 구매 내역을 볼 수 있습니다.
 				layout.showReservedMovies()
 			case "4":
-				// TODO: 팝콘, 음료 등 구매
-				print("푸드 구매")
+				// 사용자가 4 입력 시 [Layout] 스낵 메뉴가 나타납니다.
+				layout.showSnacks()
 			case "5":
 				print(manual.sleep)
 				break
