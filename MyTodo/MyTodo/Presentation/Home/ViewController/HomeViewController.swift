6df451d14827772d4029c21716eb405b5c488b93
yujinkim1 <yujin7621@me.com>
2023-08-07 00:21:26 +0900

    add: Home navigation controller

--- MyTodo/MyTodo/Presentation/Home/ViewController/HomeViewController.swift
+++ MyTodo/MyTodo/Presentation/Home/ViewController/HomeViewController.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		title = "MyTodo"
 	}
-
-
+	
 }
 
