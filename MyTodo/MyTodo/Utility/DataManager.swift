ed2233e4583bbe4a634b5d5145e0c303ed8d72c7
yujinkim1 <yujin7621@me.com>
2023-08-08 11:36:21 +0900

    add: Model data manager

---
+++ MyTodo/MyTodo/Utility/DataManager.swift
@@ -0,0 +1,18 @@
+//
+//  DataManager.swift
+//  MyTodo
+//
+//  Created by Yujin Kim on 2023-08-08.
+//
+
+import Foundation
+
+/// 싱글톤 패턴으로 하나의 인스턴스만 사용 및 유지
+class DataManager {
+	static let shared = DataManager()
+	
+	private init() {}
+	
+	var todoList: [Todo] = []
+	var completedList: [Completed] = []
+}
