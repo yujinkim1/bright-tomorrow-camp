ae39b115fbb39811a195425c89d11febd5013aa2
yujinkim1 <yujin7621@me.com>
2023-08-08 10:38:46 +0900

    modified: Action included table view cell

--- MyTodo/MyTodo/Presentation/TodoList/ViewController/TodoListViewController.swift
+++ MyTodo/MyTodo/Presentation/TodoList/ViewController/TodoListViewController.swift
@@ -13,7 +13,6 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
 	private var completeList: [Completed] = []
 	
 	@IBOutlet weak var todoListTableView: UITableView!
-	
 	@IBAction func addTodoButton(_ sender: UIBarButtonItem) {
 		let alretController = UIAlertController(title: "추가하기", message: nil, preferredStyle: .alert)
 		alretController.addTextField { textField in
@@ -59,12 +58,12 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
 		todoListTableView.delegate = self
 		todoListTableView.dataSource = self
 	}
-
+	
 	private func setTestTodo() {
 		self.todoList = [
 			Todo(
 				id: 1,
-				content: "테스트 셀입니다.",
+				content: "스위프트 문법 공부하기",
 				createdTime: DatePrinter.createTime(),
 				isCompleted: false
 			)
@@ -76,22 +75,18 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
 		todoList[indexPath.row].isCompleted = isSelected
 		todoListTableView.reloadRows(at: [indexPath], with: .automatic)
 	}
-
+	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return todoList.count
 	}
-
+	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCell
 		let todo = todoList[indexPath.row]
 		cell.configure(with: todo)
-		
-		cell.checkboxTapHandler = { [weak self] isSelected in
-			self?.updateTodoCompletionStatus(at: indexPath, isSelected: isSelected)
-		}
 		return cell
 	}
-
+	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		var selected = todoList[indexPath.row]
 		selected.isCompleted.toggle()
@@ -102,26 +97,22 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
 //TODO: TodoListTableView 분리할 것
 //class TodoListTableView: UITableViewDelegate, UITableViewDataSource {}
 
-//MARK: TodoList에서 사용하는 Cell Life-cycle
+//MARK: TodoList에서 사용하는 셀 라이프 사이클
 class TodoCell: UITableViewCell {
 	
 	static let reuseIdenifier = "TodoCell"
-	private var checkboxImage: UIImageView!
-	private var createdTimeLabel: UILabel!
-	var checkboxTapHandler: ((Bool) -> Void)?
-	
+	private var checkboxImage: UIImage!
+	@IBOutlet weak var contentLabel: UILabel!
+	@IBOutlet weak var createTimeLabel: UILabel!
+	@IBOutlet weak var checkboxButton: UIButton!
+	@IBAction func onTapCheckBoxButton(_ sender: UIButton) {
+		checkboxButton.isSelected.toggle()
+		checkboxButton.setImage(UIImage(systemName: "circle.inset.filled"), for: UIControl.State.selected)
+	}
 	
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
 		setTodoCell()
-		
-		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(checkboxTapped))
-		checkboxImage.addGestureRecognizer(tapGesture)
-		checkboxImage.isUserInteractionEnabled = true
-	}
-	
-	@objc private func checkboxTapped() {
-		checkboxTapHandler?(isSelected)
 	}
 	
 	@available(*, unavailable)
@@ -132,21 +123,11 @@ class TodoCell: UITableViewCell {
 	func setTodoCell() {
 		accessoryType = .none
 		selectionStyle = .default
-		createdTimeLabel = UILabel(frame: CGRect(x: 353.0, y: 14.0, width: 353.0, height: 14.0))
-		createdTimeLabel.textColor = UIColor.systemGray
-		addSubview(createdTimeLabel)
 	}
 	
 	func configure(with todo: Todo) {
-		textLabel?.text = todo.content
-		imageView?.image = updateCheckboxImage(isCompleted: todo.isCompleted)
-		imageView?.tintColor = UIColor.black
-		createdTimeLabel?.text = todo.createdTime
+		contentLabel.text = todo.content
+		createTimeLabel.text = todo.createdTime
 	}
 	
-	func updateCheckboxImage(isCompleted: Bool) -> UIImage {
-		let image = isCompleted ? "circle.inset.filled" : "circle"
-		return UIImage(systemName: image)!
-	}
-
 }
