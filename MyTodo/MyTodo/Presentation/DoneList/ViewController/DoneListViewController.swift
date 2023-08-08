374e5cce43e526954e6e993c0acf9f8f88259b99
yujinkim1 <yujin7621@me.com>
2023-08-09 01:08:02 +0900

    add: Loaded done list table view data

--- MyTodo/MyTodo/Presentation/DoneList/ViewController/DoneListViewController.swift
+++ MyTodo/MyTodo/Presentation/DoneList/ViewController/DoneListViewController.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
-class DoneListViewController: UIViewController {
-
+class DoneListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	
+	//MARK: Interface Builder 아웃렛
+	@IBOutlet weak var doneListTableView: UITableView!
+	
+	//MARK: UI View 라이프 사이클
     override func viewDidLoad() {
         super.viewDidLoad()
 		title = "완료됨"
-        // Do any additional setup after loading the view.
+		setDoneListTableView()
     }
-    
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+	}
+	
+	private func setDoneListTableView() {
+		doneListTableView.dataSource = self
+		doneListTableView.delegate = self
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return DataManager.shared.doneList.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = doneListTableView.dequeueReusableCell(withIdentifier: "DoneCell", for: indexPath) as! DoneCell
+		let done = DataManager.shared.doneList[indexPath.row]
+		cell.configure(with: done)
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			DataManager.shared.doneList.remove(at: indexPath.row)
+			doneListTableView.deleteRows(at: [indexPath], with: .fade)
+		}
+	}
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//MARK: DoneList에서 사용하는 셀 라이프 사이클
+class DoneCell: UITableViewCell {
+	
+	static let reuseIdentifier = "DoneCell"
+	
+	@IBOutlet weak var fixedCircleImageView: UIButton!
+	@IBOutlet weak var contentLabel: UILabel!
+	@IBOutlet weak var dueDateLabel: UILabel!
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+		setDoneCell()
+	}
+	
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	func setDoneCell() {
+		accessoryType = .none
+		selectionStyle = .none
+	}
+	
+	func configure(with done: Done) {
+		contentLabel.text = done.content
+		dueDateLabel.text = done.dueDate
+	}
 }
