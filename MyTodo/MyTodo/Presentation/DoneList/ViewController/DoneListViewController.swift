2c3344c66e372269cc4fb5fdf1e26d4f3833a3f3
yujinkim1 <yujin7621@me.com>
2023-08-08 12:41:57 +0900

    modified: View title

--- MyTodo/MyTodo/Presentation/DoneList/ViewController/DoneListViewController.swift
+++ MyTodo/MyTodo/Presentation/DoneList/ViewController/DoneListViewController.swift
@@ -11,7 +11,7 @@ class DoneListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		title = "완료됨"
         // Do any additional setup after loading the view.
     }
     
