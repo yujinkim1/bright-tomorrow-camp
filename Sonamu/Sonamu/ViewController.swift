a632b0b514b6ca55311af239fe870b467bb5c3e0
yujinkim1 <yujin7621@me.com>
2023-08-22 22:42:07 +0900

    add: 소나무 프로젝트

---
+++ Sonamu/Sonamu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Sonamu/SonamuUITests/SonamuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SonamuUITestsLaunchTests.swift
+//  SonamuUITests
+//
+//  Created by Yujin Kim on 2023-08-22.
+//
+
+import XCTest
+
+final class SonamuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
