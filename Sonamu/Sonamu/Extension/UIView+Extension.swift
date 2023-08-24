da643c044b80fdb6d42b1bdb2a1e72c1872903ab
yujinkim1 <yujin7621@me.com>
2023-08-24 23:48:31 +0900

    add: UIView extension

---
+++ Sonamu/Sonamu/Extension/UIView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Extension.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-23.
+//
+
+import UIKit
+
+extension UIView {
+	func addSubviews(_ views: [UIView]) {
+		views.forEach { self.addSubview($0) }
+	}
+}
