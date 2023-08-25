89823f33cd966f4721cc5c89d5f69d5a2ac84014
yujinkim1 <yujin7621@me.com>
2023-08-25 21:03:11 +0900

    add: Home view model

---
+++ Sonamu/Sonamu/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-23.
+//
+
+import Foundation
+
+//TODO: - 데이터 흐름 수립 후 구현 필요
+class HomeViewModel: NSObject {
+	//MARK: - 데이터
+	
+	//MARK: - 데이터가 비어있을 경우
+	
+	//MARK: - 데이터가 비어있지 않고 하나라도 있을 경우
+}
