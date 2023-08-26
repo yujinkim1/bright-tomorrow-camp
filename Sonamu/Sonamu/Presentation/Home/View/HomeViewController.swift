e3f2a704b1c16d62a37de11a5474718693fe53a3
yujinkim1 <yujin7621@me.com>
2023-08-26 22:44:05 +0900

    modified: 뷰 모델 연결

--- Sonamu/Sonamu/Presentation/Home/View/HomeViewController.swift
+++ Sonamu/Sonamu/Presentation/Home/View/HomeViewController.swift
@@ -8,25 +8,41 @@
 import UIKit
 
 class HomeViewController: BaseUIViewController {
-	var coordinator: HomeViewCoordinatorImpl?
+	//MARK: - ViewModel
+	var viewModel: HomeViewModel
 	
 	//MARK: - UI Components
 	
 	private lazy var homeView = UIView()
 	
 	private lazy var titleLabel: UILabel = {
-		let label: UILabel = UILabel()
+		let label = UILabel()
 		label.frame = CGRect(x: 0, y: 0, width: 100, height: 45)
 		label.font = UIFont(name: "SUITE-Bold", size: 38.0)
-		label.text = "목록"
+		label.text = "둘러보기"
 		label.textAlignment = .left
 		return label
 	}()
 	
-	private lazy var addCardButton: UIButton = {
-		let button: UIButton = UIButton()
-		button.setTitle("추가", for: .normal)
-		button.backgroundColor = .black
+	private lazy var tableView: UITableView = {
+		let tableView = UITableView()
+		return tableView
+	}()
+	
+	private lazy var createButton: UIButton = {
+		let button = UIButton()
+		button.backgroundColor = .white
+		button.setTitle("다이어리 생성하기", for: .normal)
+		button.setTitleColor(.black, for: .normal)
+		button.titleLabel?.font = UIFont(name: "SUITE-Medium", size: 24.0)
+		button.titleLabel?.textAlignment = .center
+		button.layer.cornerRadius = CGFloat(30)
+		button.layer.shadowOffset = CGSize(width: 0, height: 4)
+		button.layer.shadowColor = UIColor.gray.cgColor
+		button.layer.shadowOpacity = 0.5
+		button.layer.shadowRadius = CGFloat(12)
+		button.layer.masksToBounds = false
+		button.isEnabled = true
 		return button
 	}()
 	
@@ -34,13 +50,14 @@ class HomeViewController: BaseUIViewController {
 	
 	override func setUI() {
 		view.addSubview(homeView)
-		homeView.addSubviews([titleLabel])
+		homeView.addSubviews([titleLabel, tableView, createButton])
 	}
 	
 	override func setLayout() {
 		homeView.translatesAutoresizingMaskIntoConstraints = false
 		titleLabel.translatesAutoresizingMaskIntoConstraints = false
-		addCardButton.translatesAutoresizingMaskIntoConstraints = false
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		createButton.translatesAutoresizingMaskIntoConstraints = false
 		
 		NSLayoutConstraint.activate([
 			//homeView(l, t, r, b)
@@ -51,13 +68,40 @@ class HomeViewController: BaseUIViewController {
 			//titleLabel(l, t, r, b)
 			titleLabel.leadingAnchor.constraint(equalTo: homeView.leadingAnchor, constant: 10),
 			titleLabel.topAnchor.constraint(equalTo: homeView.topAnchor, constant: 60),
+			//collectionView(l, t, r, b)
+			tableView.leadingAnchor.constraint(equalTo: homeView.leadingAnchor, constant: 0),
+			tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0),
+			tableView.trailingAnchor.constraint(equalTo: homeView.trailingAnchor, constant: 0),
+			tableView.bottomAnchor.constraint(equalTo: createButton.topAnchor, constant: 0),
+			//addCardButton(l, t, r, b, w, h)
+			createButton.leadingAnchor.constraint(equalTo: homeView.leadingAnchor, constant: 0),
+			createButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+			createButton.trailingAnchor.constraint(equalTo: homeView.trailingAnchor, constant: 0),
+			createButton.heightAnchor.constraint(equalToConstant: 120)
 		])
+	}
+	
+	override func setDelegate() {
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
+		tableView.dataSource = self
+		tableView.delegate = self
 	}
 	
 	override func addTarget() {
 		let swipeToLeftGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeToLeft(_:)))
 		swipeToLeftGesture.direction = .left
 		view.addGestureRecognizer(swipeToLeftGesture)
+		createButton.addTarget(self, action: #selector(showCreateViewController(_:)), for: .touchUpInside)
+	}
+	
+	init(viewModel: HomeViewModel) {
+		self.viewModel = viewModel
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	@available(*, unavailable)
+	required init?(coder _: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
 	}
 	
 	//MARK: - Custom Action
@@ -65,7 +109,47 @@ class HomeViewController: BaseUIViewController {
 	@objc
 	private func handleSwipeToLeft(_ gestureRecognizer: UISwipeGestureRecognizer) {
 		if gestureRecognizer.state == .ended {
-			coordinator?.showCreateViewController()
+			viewModel.coordinator?.showCreateViewController()
 		}
 	}
+	
+	@objc
+	private func showCreateViewController(_ button: UIButton) {
+		viewModel.coordinator?.showCreateViewController()
+	}
 }
+
+//MARK: - 테이블 뷰
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return viewModel.setNumberOfRowsInSection()
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCell
+		let todo = viewModel.todos[indexPath.row]
+		cell.configure(with: todo)
+		return cell
+	}
+}
+
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+private struct UIViewControllerRepresenter: UIViewControllerRepresentable {
+	let viewController: UIViewController
+	
+	func makeUIViewController(context: Context) -> UIViewController {
+		return viewController
+	}
+	
+	func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct UIViewControllerPreviewViews: PreviewProvider {
+	static var previews: some View {
+		let viewModel = HomeViewModel()
+		let viewController = HomeViewController(viewModel: viewModel)
+		return UIViewControllerRepresenter(viewController: viewController)
+	}
+}
+#endif
