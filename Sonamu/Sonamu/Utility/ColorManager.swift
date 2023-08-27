bfc245cee6e8dac1c9f2ff6bf21d1c6522310edf
yujinkim1 <yujin7621@me.com>
2023-08-27 17:52:02 +0900

    add: Color manager

---
+++ Sonamu/Sonamu/Utility/ColorManager.swift
@@ -0,0 +1,15 @@
+//
+//  ColorManager.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-25.
+//
+
+import UIKit
+
+class ColorManager {
+	static let kPrimaryColor = UIColor(named: "snmPrimaryColor")
+	static let kSecondaryColor = UIColor(named: "snmSecondaryColor")
+	static let kErrorColor = UIColor(named: "snmErrorColor")
+	static let kBackgroundColor = UIColor(named: "snmBackgroundColor")
+}
