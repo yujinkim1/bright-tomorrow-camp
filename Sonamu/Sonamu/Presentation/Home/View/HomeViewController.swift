42b34b545b7f7482143c67ae1220ef173d96eaea
yujinkim1 <yujin7621@me.com>
2023-08-30 23:27:41 +0900

    add: TODO 코멘트

--- Sonamu/Sonamu/Presentation/Home/View/HomeViewController.swift
+++ Sonamu/Sonamu/Presentation/Home/View/HomeViewController.swift
@@ -8,84 +8,75 @@
 import UIKit
 
 class HomeViewController: BaseUIViewController {
-	//MARK: - Coordinator
+	//MARK: - 코디네이터
 	weak var coordinator: HomeViewCoordinatorImpl?
 	
-	//MARK: - ViewModel
+	//MARK: - 뷰 모델
 	var viewModel: HomeViewModel?
 	
-	//MARK: - UI Components
+	//TODO: - Todo 뷰 모델로 이동 해야함
+	var decodedTodoGroup: [Todo] = []
+	
+	//MARK: - UI 컴포넌트
+	private lazy var createTodoButton: UIBarButtonItem = {
+		let barButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: #selector(createTodoButtonTapped(_:)))
+		barButtonItem.isEnabled = true
+		return barButtonItem
+	}()
+	
 	private lazy var tableView: UITableView = {
-		let tableView = UITableView()
-		tableView.backgroundColor = ColorManager.kErrorColor
-		tableView.backgroundView = emptyLabel
-		tableView.separatorStyle = .none
+		let tableView = UITableView(frame: .zero, style: .insetGrouped)
+		tableView.backgroundColor = .systemGray6
+		tableView.rowHeight = 100
+		tableView.separatorStyle = .singleLine
+		tableView.layer.cornerRadius = CGFloat(8)
 		return tableView
 	}()
 	
-	private lazy var emptyLabel: UILabel = {
-		let label = UILabel()
-		label.textAlignment = .center
-		label.textColor = ColorManager.kSecondaryColor
-		label.text = viewModel?.setEmptyLiteral()
-		return label
-	}()
-	//TODO: - 나중에 날씨 받아오는거 넣어볼 것
-	private lazy var flexibleLabel: UILabel = {
-		let label = UILabel()
-		label.text = "날씨"
-		label.font = UIFont(name: "SUITE-Medium", size: 32.0)
-		label.textAlignment = .left
-		return label
-	}()
-	
-	private lazy var createTodoButton: UIButton = {
-		let button = UIButton(type: .roundedRect)
-		button.backgroundColor = .white
-		button.setTitle("할 일 추가", for: .normal)
-		button.setTitleColor(.black, for: .normal)
-		button.titleLabel?.textAlignment = .center
-		button.layer.cornerRadius = button.bounds.size.width / 2
-		button.clipsToBounds = true
-		button.layer.shadowOffset = CGSize(width: 0, height: 4)
-		button.layer.shadowColor = UIColor.gray.cgColor
-		button.layer.shadowOpacity = 0.5
-		button.layer.shadowRadius = CGFloat(8)
-		button.layer.masksToBounds = false
-		button.isEnabled = true
-		return button
+	private lazy var catPhotoView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.contentMode = .scaleAspectFit
+		imageView.layer.cornerRadius = CGFloat(16)
+		imageView.clipsToBounds = true
+		return imageView
 	}()
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
+		self.decodedTodoGroup = viewModel?.loadTodoGroupItems() ?? []
+		catPhotoView.image = UIImage(named: "imageNotFoundImage")
+		viewModel?.catService.download { [weak self] (image) in
+			DispatchQueue.main.async {
+				if let image = image {
+					self?.catPhotoView.image = image
+				} else {
+					print("Failed to download cat image.")
+					self?.catPhotoView.image = UIImage(named: "imageNotFoundImage")
+				}
+			}
+		}
 		tableView.reloadData()
 	}
 	//MARK: - Custom Method
 	
 	override func setUI() {
-		view.addSubviews([tableView, flexibleLabel, createTodoButton])
+		view.addSubviews([catPhotoView, tableView])
+		navigationItem.rightBarButtonItem = createTodoButton
 	}
 	
 	override func setLayout() {
+		catPhotoView.translatesAutoresizingMaskIntoConstraints = false
 		tableView.translatesAutoresizingMaskIntoConstraints = false
-		flexibleLabel.translatesAutoresizingMaskIntoConstraints = false
-		createTodoButton.translatesAutoresizingMaskIntoConstraints = false
 		
 		NSLayoutConstraint.activate([
-			//tableView(l, t, r, b)
+			catPhotoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+			catPhotoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			catPhotoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+			catPhotoView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.4),
 			tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-			tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-			tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-			tableView.bottomAnchor.constraint(equalTo: createTodoButton.topAnchor, constant: 0),
-			//flexibleLabel(l, t, r, b)
-			flexibleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-			flexibleLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-			flexibleLabel.trailingAnchor.constraint(equalTo: createTodoButton.trailingAnchor),
-			flexibleLabel.heightAnchor.constraint(equalToConstant: 80),
-			//createTodoButton(l, t, r, b, w, h)
-			createTodoButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-			createTodoButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-			createTodoButton.heightAnchor.constraint(equalToConstant: 80)
+			tableView.topAnchor.constraint(equalTo: catPhotoView.bottomAnchor, constant: 10),
+			tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+			tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
 		])
 	}
 	
@@ -94,14 +85,13 @@ class HomeViewController: BaseUIViewController {
 	}
 	
 	override func setDelegate() {
-		tableView.register(TodoCell.self, forCellReuseIdentifier: "TodoCell")
+		tableView.register(TodoGroupCell.self, forCellReuseIdentifier: "TodoGroupCell")
+		tableView.register(DoneGroupCell.self, forCellReuseIdentifier: "DoneGroupCell")
 		tableView.dataSource = self
 		tableView.delegate = self
 	}
 	
-	override func addTarget() {
-		createTodoButton.addTarget(self, action: #selector(createTodoButtonTapped(_:)), for: .touchUpInside)
-	}
+	override func addTarget() {}
 	
 	//MARK: - Custom Action
 	@objc
@@ -110,15 +100,69 @@ class HomeViewController: BaseUIViewController {
 	}
 }
 
-//MARK: - 테이블 뷰
+//MARK: - UITableView
 extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+	//TODO: - TodoViewController / DoneViewController 이동 가능 셀로 변경해야함
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
+		if section == 0 {
+			let count = viewModel?.loadTodoGroupItemCount()
+			return count ?? 0
+		} else {
+			let count = viewModel?.loadDoneGroupItemCount()
+			return count ?? 0
+		}
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if indexPath.section == 0 {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "TodoGroupCell", for: indexPath) as! TodoGroupCell
+			cell.titleLabel.text = decodedTodoGroup[indexPath.row].content
+			return cell
+		} else {
+			let cell = tableView.dequeueReusableCell(withIdentifier: "DoneGroupCell", for: indexPath) as! DoneGroupCell
+			cell.titleLabel.text = "UserDefaults에 저장됨"
+			return cell
+		}
 	}
 	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCell
-		return cell
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		switch indexPath.section {
+			case 1: coordinator?.toDoneViewController()
+			default: break
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+		let headerTitleLabel = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.bounds.size.width, height: tableView.sectionHeaderHeight))
+		headerTitleLabel.font = UIFont(name: "SUITE-ExtraBold", size: 26.0)
+		headerTitleLabel.textColor = ColorManager.kPrimaryColor
+		if section == 0 {
+			headerTitleLabel.text = "Todo"
+		} else if section == 1 {
+			headerTitleLabel.text = "Done"
+		}
+		return headerTitleLabel
+	}
+	
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		if section == 0 {
+			return "Todo"
+		} else if section == 1 {
+			return "Done"
+		}
+		return nil
+	}
+	
+	func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+		if section == 0 {
+			return "\(String(describing: viewModel?.loadTodoGroupItemCount()))개의 항목"
+		} else if section == 1 {
+			return "\(String(describing: viewModel?.loadDoneGroupItemCount()))개의 항목"
+		}
+		return nil
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
 	}
 }
 
