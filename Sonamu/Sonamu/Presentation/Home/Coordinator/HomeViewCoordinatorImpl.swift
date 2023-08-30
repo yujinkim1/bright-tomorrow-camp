61b81935eb4637c284e5e3d1fc05fb630f50ca95
yujinkim1 <yujin7621@me.com>
2023-08-30 21:19:56 +0900

    add: TODO 코멘트

--- Sonamu/Sonamu/Presentation/Home/Coordinator/HomeViewCoordinatorImpl.swift
+++ Sonamu/Sonamu/Presentation/Home/Coordinator/HomeViewCoordinatorImpl.swift
@@ -21,13 +21,26 @@ class HomeViewCoordinatorImpl: Coordinator {
 		navigationController.pushViewController(viewController, animated: false)
 	}
 	
+	//TODO: - TodoViewCoordinatorImpl 코디네이터 클래스로 이동해야함.
+	func toCreateTodoViewController() {
+		let viewController = CreateTodoViewController()
+		viewController.viewModel = CreateTodoViewModel()
+		let navigationController = UINavigationController(rootViewController: viewController)
+		navigationController.modalPresentationStyle = .fullScreen
+		self.navigationController.present(navigationController, animated: true)
+	}
+	
+	//TODO: - TodoViewController ViewModel
 	func toTodoViewController() {
 		let viewController = TodoViewController()
+		//viewController.viewModel = TodoViewModel()
 		navigationController.pushViewController(viewController, animated: true)
 	}
 	
+	//TODO: - DoneViewController ViewModel
 	func toDoneViewController() {
 		let viewController = DoneViewController()
+		//viewController.viewModel = DoneViewModel()
 		navigationController.pushViewController(viewController, animated: true)
 	}
 }
