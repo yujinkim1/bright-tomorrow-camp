8b7e8ceddb5da57e4ee8871bee69bfd7a97c281c
yujinkim1 <yujin7621@me.com>
2023-09-01 00:07:15 +0900

    add: 코디네이터

---
+++ Sonamu/Sonamu/Presentation/Todo/Coordinator/TodoViewCoordinatorImpl.swift
@@ -0,0 +1,26 @@
+//
+//  TodoViewCoordinatorImpl.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-31.
+//
+
+import UIKit
+
+class TodoViewCoordinatorImpl: Coordinator {
+	var navigationController: UINavigationController
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+	
+	func start() {}
+	
+	func toCreateTodoViewController() {
+		let viewController = CreateTodoViewController()
+		viewController.viewModel = CreateTodoViewModel()
+		let newNavigationController = UINavigationController(rootViewController: viewController)
+		newNavigationController.modalPresentationStyle = .fullScreen
+		navigationController.present(newNavigationController, animated: true)
+	}
+}
