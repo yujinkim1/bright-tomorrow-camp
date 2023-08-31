d63b95d51d964e45b44b7cb7d455238e5801a8e3
yujinkim1 <yujin7621@me.com>
2023-08-31 23:52:40 +0900

    add: UserDefaults 매니저

---
+++ Sonamu/Sonamu/Utility/UserDefaultsManager.swift
@@ -0,0 +1,25 @@
+//
+//  UserDefaultsManager.swift
+//  Sonamu
+//
+//  Created by Yujin Kim on 2023-08-29.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+	static let defaults = UserDefaults.standard
+	
+	private init() {}
+	
+	static func loadGroup<T>(key: String, type: T.Type) -> T? where T: Decodable {
+		guard let data = UserDefaultsManager.defaults.data(forKey: key) else { return nil }
+		do {
+			let group = try JSONDecoder().decode(T.self, from: data)
+			return group
+		} catch {
+			print(error.localizedDescription)
+			return nil
+		}
+	}
+}
